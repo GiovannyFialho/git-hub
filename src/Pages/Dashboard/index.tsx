79a4e733477ef281407380940c71a4bedd45f55e
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2020-12-31 10:42:39 -0300

    persistindo dados encontrados

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState, useEffect, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/github-logo.svg'
@@ -18,7 +18,20 @@ interface Repository {
 const Dashboad: React.FC = () => {
     const [newRepo, setNewRepo] = useState('')
     const [inputError, setInputError] = useState('')
-    const [repositories, setRepositories] = useState<Repository[]>([])
+
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories)
+        }
+
+        return []
+    })
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
 
     async function handleAddRepository (event: FormEvent<HTMLFormElement>):Promise<void> {
         event.preventDefault()
